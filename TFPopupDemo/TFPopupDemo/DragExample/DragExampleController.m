6e4c718a8e7f4fce13f8021794a5ac5d5995f3f3
zhutaofeng <zhutaofeng@sdiholding.com>
2019-11-18 14:18:47 +0800

    添加拖拽示例和demo代码

--- TFPopupDemo/TFPopupDemo/DragExample/DragExampleController.m
+++ TFPopupDemo/TFPopupDemo/DragExample/DragExampleController.m
@@ -7,7 +7,7 @@
 //
 
 #import "DragExampleController.h"
-
+#import "TFPopup.h"
 @interface DragExampleController ()
 
 @end
@@ -22,5 +22,42 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (IBAction)showTop:(id)sender {
+    
+    UIImage *image = [UIImage imageNamed:@"popup-11"];
+    CGRect bounds = [UIScreen mainScreen].bounds;
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, bounds.size.width, bounds.size.width / (image.size.width / image.size.height))];
+    imageView.userInteractionEnabled = YES;
+    imageView.image = image;
+    
+    TFPopupParam *param = [TFPopupParam new];
+    param.backgroundColor = [[UIColor blackColor]colorWithAlphaComponent:0.6];
+    param.disuseShowPopupAlphaAnimation = YES;
+    param.disuseHidePopupAlphaAnimation = YES;
+    param.dragEnable = YES;
+    param.dragBouncesEnable = YES;
+    param.dragAutoDissmissMinDistance = 60;
+    [imageView tf_showSlide:self.view direction:PopupDirectionTop popupParam:param];
+}
+
+
+- (IBAction)showBottom:(id)sender {
+    UIImage *image = [UIImage imageNamed:@"popup-10"];
+
+    CGRect bounds = [UIScreen mainScreen].bounds;
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, bounds.size.width, bounds.size.width / (image.size.width / image.size.height))];
+    imageView.userInteractionEnabled = YES;
+    imageView.image = image;
+
+    TFPopupParam *param = [TFPopupParam new];
+    param.backgroundColor = [[UIColor blackColor]colorWithAlphaComponent:0.6];
+    param.disuseShowPopupAlphaAnimation = YES;
+    param.disuseHidePopupAlphaAnimation = YES;
+    param.dragEnable = YES;
+    param.dragBouncesEnable = YES;
+    param.dragStyle = DragStyleToLeft | DragStyleToRight | DragStyleToBottom;
+    param.dragAutoDissmissMinDistance = 150;
+    [imageView tf_showSlide:self.view direction:PopupDirectionBottom popupParam:param];
+}
 
 @end
